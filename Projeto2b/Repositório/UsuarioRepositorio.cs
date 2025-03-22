7b58dadb34c6ea995eb2ba8be276e3e12560e8d2
Marina <msc.candizani@gmail.com>
2025-03-21 22:14:57 -0300

    Criando o metodo adicionar e obter o usuario

--- Projeto2b/Repositório/UsuarioRepositorio.cs
+++ Projeto2b/Repositório/UsuarioRepositorio.cs
@@ -1,6 +1,41 @@
-﻿namespace Projeto2b.Repositório
+﻿using Projeto2b.Models;
+
+namespace Projeto2b.Repositório
 {
     public class UsuarioRepositorio
     {
+
+        // Preparando a estrutura da conexao 
+
+        private readonly string _connectionString;
+
+        // Construtor
+        public UsuarioRepositorio(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        // Criando o metodo adicionar usuario 
+
+        public void AdicionarUsuario(Usuario usuario)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "INSERT INTO Usuario (Nome, Email, Senha) VALUES (@Nome,@Email,@Senha)";
+                cmd.Parameters.AddWithValue("@Nome", usuario.Nome);
+                cmd.Parameters.AddWithValue("@Email", usuario.Email);
+                cmd.Parameters.AddWithValue("@Senha", usuario.Senha);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+
+
+
+
+
+
     }
 }
